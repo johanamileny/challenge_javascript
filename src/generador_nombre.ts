998e8f593ff7794c47eea0a8d3615b72e5d0b297
johanamileny <johanap687@gmail.com>
2024-07-10 11:09:16 -0500

    modifica codigo a generardor de nombres

--- src/generador_nombre.ts
+++ src/generador_nombre.ts
@@ -2,21 +2,22 @@ class Usuario {
     name: string;
     lastname: string;
 
-    constructor(name: string, lastname: string){
-           this.name =  name
-           this.lastname = lastname
+    constructor(name: string, 
+              lastname: string){
+              this.name =  name
+              this.lastname = lastname
            
     }
 
           aleatorio():number {
 
-           return Math.floor(Math.random() * 10) + 1; /* metodo crea numerosaleatorios */
+           return Math.floor(Math.random() * 10) + 1;
     
           } 
           generdornameu(): string {
 
-           const tresname = this.name.substring(0, 3);  /*sustrae las tres primeras lectras del nombre  */
-           const treslastname = this.lastname.substring(0, 3);/*sustrae las tres primeras lectras del apellido  */
+           const tresname = this.name.substring(0, 3); 
+           const treslastname = this.lastname.substring(0, 3);
            const aleatorinumber = this.aleatorio()
 
            return tresname + treslastname + aleatorinumber;
